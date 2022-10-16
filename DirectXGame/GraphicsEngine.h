0f2481521c435083b2b94a644bc2867adaf73838
Emerson Celestial <80930588+EmersonicsDLSU@users.noreply.github.com>
2022-10-17 07:29:15 +0800

    Initialize Pasrt15_ImprovingEngineArchitecture

---
+++ DirectXGame/GraphicsEngine.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <d3d11.h>
+#include "Prerequisites.h"
+#include "RenderSystem.h"
+
+// Singleton class
+// initializes the RenderSystem
+class GraphicsEngine
+{
+public:
+	GraphicsEngine();
+	//Initialize the GraphicsEngine and DirectX 11 Device
+	bool init();
+	//Release all the resources loaded
+	bool release();
+	RenderSystem* getRenderSystem();
+	~GraphicsEngine();
+public:
+	static GraphicsEngine* get();
+private:
+	RenderSystem* m_render_system = nullptr;
+};
+
---
+++ DirectXGame/IndexBuffer.h
@@ -0,0 +1,25 @@
+
+#pragma once
+#include <d3d11.h>
+#include "Prerequisites.h"
+
+/*
+ * Because indices need to be accessed by the GPU, they
+ * need to be placed in a special resource structure: an
+ * index buffer. Creating an index buffer is very similar
+ * to creating a vertex buffer, except that the index buffer
+ * stores indices instead of vertices.*/
+class IndexBuffer
+{
+public:
+	IndexBuffer(void* list_indices, UINT size_list, RenderSystem* system);
+	UINT getSizeIndexList();
+	~IndexBuffer();
+private:
+	UINT m_size_list;
+private:
+	ID3D11Buffer* m_buffer;
+	RenderSystem* m_system = nullptr;
+private:
+	friend class DeviceContext;
+};
