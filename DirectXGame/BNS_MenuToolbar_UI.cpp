c55e7810487b63277912d263236545f869a1c7aa
EmersonicsDLSU <emerson_celestial@dlsu.edu.ph>
2022-11-06 22:48:12 +0800

    Upd ViewMode

--- DirectXGame/BNS_MenuToolbar_UI.cpp
+++ DirectXGame/BNS_MenuToolbar_UI.cpp
@@ -1,4 +1,7 @@
 #include "BNS_MenuToolbar_UI.h"
+
+#include "BNS_Camera.h"
+#include "BNS_CameraHandler.h"
 #include "BNS_UIManager.h"
 
 BNS_MenuToolbar_UI::BNS_MenuToolbar_UI(std::string name) : BNS_AUIScreen(name)
@@ -18,4 +21,19 @@ void BNS_MenuToolbar_UI::DrawUI()
 
 		ImGui::EndMainMenuBar();
 	}
+
+	//DRAW HERE
+	ImGui::Begin("View Mode");
+	
+	bool temp = dynamic_cast<BNS_Camera*>(BNS_CameraHandler::GetInstance()->GetSceneCamera().get())->IsPerspectiveMode();
+	if (ImGui::Button("IsPerspective"))
+	{
+		dynamic_cast<BNS_Camera*>(BNS_CameraHandler::GetInstance()->GetSceneCamera().get())->SetPerspectiveMode(!temp);
+	}
+	if (temp)
+		ImGui::Text("Perspective Mode");
+	else
+		ImGui::Text("Orthographic Mode");
+
+	ImGui::End();
 }
